0bedfe929a7f778a94ed3477fda8c85bce73f12a
Andreas Meingast <ameingast@gmail.com>
2013-02-20 16:25:05 +0100

    Fixed str_append bug + indentation

--- src/bm.c
+++ src/bm.c
@@ -6,118 +6,118 @@
 #include "libstr.h"
 
 #ifndef BM_CNT
-  #define BM_CNT 100000
+#define BM_CNT 100000
 #endif
 
 #define BM(NAME, FUN) BMI(NAME, __i, FUN)
 
 #define BMI(NAME, I, FUN) do {                                  \
-  clock_t t0 = clock();                                         \
-  for (int I = 0; I < BM_CNT; I ++) {FUN}                       \
-  clock_t t1 = clock();                                         \
-  (void)printf("%s:\t%Fms\n", NAME, (double)(t1 - t0)/BM_CNT);  \
+    clock_t t0 = clock();                                         \
+    for (int I = 0; I < BM_CNT; I ++) {FUN}                       \
+    clock_t t1 = clock();                                         \
+    (void)printf("%s:\t%Fms\n", NAME, (double)(t1 - t0)/BM_CNT);  \
 } while (0)
 
 static inline void bm_str_alloc(void)
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  BMI("str_alloc()", i, { ss[i] = str_alloc(); });
-  for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
+    str_t *ss[BM_CNT] = { NULL };
+
+    BMI("str_alloc()", i, { ss[i] = str_alloc(); });
+    for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
 }
 
 static inline void bm_str_alloca()
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  BMI("str_alloca()", i, { ss[i] = str_alloca(); });
+    str_t *ss[BM_CNT] = { NULL };
+
+    BMI("str_alloca()", i, { ss[i] = str_alloca(); });
 }
 
 static inline void bm_str_alloc_with()
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  BMI("str_alloc_with()", i, { ss[i] = str_alloc_with("foo"); });
-  for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
+    str_t *ss[BM_CNT] = { NULL };
+
+    BMI("str_alloc_with()", i, { ss[i] = str_alloc_with("foo"); });
+    for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
 }
 
 static inline void bm_str_alloca_with()
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  BMI("str_alloca_with()", i, { ss[i] = str_alloca_with("foo"); });
+    str_t *ss[BM_CNT] = { NULL };
+
+    BMI("str_alloca_with()", i, { ss[i] = str_alloca_with("foo"); });
 }
 
 static inline void bm_str_free(void)
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc();
-  BMI("str_free()", i, { str_free(ss[i]); });
+    str_t *ss[BM_CNT] = { NULL };
+
+    for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc();
+    BMI("str_free()", i, { str_free(ss[i]); });
 }
 
 static inline void bm_str_set(void)
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc();
-  BMI("str_set(small)", i, { str_set("foo", ss[i]); });
-  BMI("str_set(big)", i, { str_set("123456789123456789123456789123456789123456789", ss[i]); });
-  for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
+    str_t *ss[BM_CNT] = { NULL };
+
+    for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc();
+    BMI("str_set(small)", i, { str_set("foo", ss[i]); });
+    BMI("str_set(big)", i, { str_set("123456789123456789123456789123456789123456789", ss[i]); });
+    for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
 }
 
 static inline void bm_str_append(void)
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc();
-  BMI("str_append(small)", i, { str_append("foo", ss[i]); });
-  BMI("str_append(big)", i, { str_append("123456789123456789123456789123456789123456789", ss[i]); });
-  BMI("str_append(loop/10)", i, { for (int j = 0; j < 10; j++) str_append("123456789123456789123456789123456789123456789", ss[i]); });
-  for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
+    str_t *ss[BM_CNT] = { NULL };
+
+    for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc();
+    BMI("str_append(small)", i, { str_append("foo", ss[i]); });
+    BMI("str_append(big)", i, { str_append("123456789123456789123456789123456789123456789", ss[i]); });
+    BMI("str_append(loop/10)", i, { for (int j = 0; j < 10; j++) str_append("123456789123456789123456789123456789123456789", ss[i]); });
+    for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
 }
 
 static inline void bm_str_append_alloca(void)
 {
-  str_t *ss[BM_CNT] = { NULL };
-  
-  for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloca();
-  BMI("A str_append(small)", i, { str_append("foo", ss[i]); });
-  BMI("A str_append(big)", i, { str_append("123456789123456789123456789123456789123456789", ss[i]); });
-  BMI("A str_append(loop/10)", i, { for (int j = 0; j < 10; j++) str_append("123456789123456789123456789123456789123456789", ss[i]); });
+    str_t *ss[BM_CNT] = { NULL };
+
+    for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloca();
+    BMI("A str_append(small)", i, { str_append("foo", ss[i]); });
+    BMI("A str_append(big)", i, { str_append("123456789123456789123456789123456789123456789", ss[i]); });
+    BMI("A str_append(loop/10)", i, { for (int j = 0; j < 10; j++) str_append("123456789123456789123456789123456789123456789", ss[i]); });
 }
 
 static inline void bm_str_join(void)
 {
-  str_t *ss[BM_CNT] = { NULL }, *aux[BM_CNT] = { NULL };
-  
-  for (int i = 0; i < BM_CNT; i++) { ss[i] = str_alloc(); str_set("foo", ss[i]); aux[i] = str_alloc(); str_set("foo", aux[i]); }
-  BMI("str_join(small)", i, { str_join(aux[i], ss[i]); });
-  for (int i = 0; i < BM_CNT; i++) { str_free(ss[i]); str_free(aux[i]); }
+    str_t *ss[BM_CNT] = { NULL }, *aux[BM_CNT] = { NULL };
+
+    for (int i = 0; i < BM_CNT; i++) { ss[i] = str_alloc(); str_set("foo", ss[i]); aux[i] = str_alloc(); str_set("foo", aux[i]); }
+    BMI("str_join(small)", i, { str_join(aux[i], ss[i]); });
+    for (int i = 0; i < BM_CNT; i++) { str_free(ss[i]); str_free(aux[i]); }
 }
 
 static inline void bm_str_length(void)
 {
-  str_t *ss[BM_CNT] = { NULL };
-  long cnt;
-  
-  for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc_with("foo");
-  BMI("str_length()", i, { cnt += str_length(ss[i]); });
-  for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
+    str_t *ss[BM_CNT] = { NULL };
+    long cnt = 0;
+
+    for (int i = 0; i < BM_CNT; i++) ss[i] = str_alloc_with("foo");
+    BMI("str_length()", i, { cnt += str_length(ss[i]); });
+    for (int i = 0; i < BM_CNT; i++) str_free(ss[i]);
 }
 
 int main(int argc, char **argv)
 {
-  bm_str_alloc();
-  bm_str_alloca();
-  bm_str_free();
-  bm_str_set();
-  bm_str_length();
-  bm_str_alloc_with();
-  bm_str_alloca_with();
-  bm_str_append();
-  bm_str_join();
-  // bm_str_append_alloca();
-  
-  return 0;
+    bm_str_alloc();
+    bm_str_alloca();
+    bm_str_free();
+    bm_str_set();
+    bm_str_length();
+    bm_str_alloc_with();
+    bm_str_alloca_with();
+    bm_str_append();
+    bm_str_join();
+    // bm_str_append_alloca();
+
+    return 0;
 }
--- src/libstr.c
+++ src/libstr.c
@@ -10,169 +10,170 @@
 #define FREE(x) do { if (x != NULL) { free(x); x = NULL; } } while (0)
 
 struct str_t_ {
-  char *s;
-  size_t len;
-  size_t actual;
-  char mem_type;
+    char *s;
+    size_t len;
+    size_t actual;
+    char mem_type;
 };
 
 inline str_t *str_alloc(void)
 {
-	str_t *str;
-	
-	if (NULL == (str = malloc(sizeof(str_t)))) {
-		return NULL;
-	}
-	if (NULL == (str->s = calloc(STR_SIZE, sizeof(char)))) {
-		FREE(str);
-		return NULL;
-	}
-	str->len = STR_SIZE;
-	str->actual = 0;
-  str->mem_type = HEAP;
-	return str;
+    str_t *str;
+
+    if (NULL == (str = malloc(sizeof(str_t)))) {
+        return NULL;
+    }
+    if (NULL == (str->s = calloc(STR_SIZE, sizeof(char)))) {
+        FREE(str);
+        return NULL;
+    }
+    str->len = STR_SIZE;
+    str->actual = 0;
+    str->mem_type = HEAP;
+    return str;
 }
 
 inline str_t *str_alloca(void)
 {
-  volatile str_t *str;
-	
-	if (NULL == (str = alloca(sizeof(str_t)))) {
-		return NULL;
-	}
-	if (NULL == (str->s = alloca(STR_SIZE * sizeof(char)))) {
-		return NULL;
-	}
-	str->len = STR_SIZE;
-	str->actual = 0;
-  str->mem_type = STACK;
-	return (str_t *)str;
+    volatile str_t *str;
+
+    if (NULL == (str = alloca(sizeof(str_t)))) {
+        return NULL;
+    }
+    if (NULL == (str->s = alloca(STR_SIZE * sizeof(char)))) {
+        return NULL;
+    }
+    str->len = STR_SIZE;
+    str->actual = 0;
+    str->mem_type = STACK;
+    return (str_t *)str;
 }
 
 inline str_t *str_alloc_with(const char *s)
 {
-  str_t *str;
-  
-  if (NULL == (str = str_alloc())) {
-    return NULL;
-  }
-  if (NULL == str_set(s, str)) {
-    return NULL;
-  }
-  return str;
+    str_t *str;
+
+    if (NULL == (str = str_alloc())) {
+        return NULL;
+    }
+    if (NULL == str_set(s, str)) {
+        return NULL;
+    }
+    return str;
 }
 
 str_t *str_alloca_with(const char *s)
 {
-  volatile str_t *str;
-  
-  if (NULL == (str = str_alloca())) {
-    return NULL;
-  }
-  if (NULL == str_set(s, (str_t *)str)) {
-    return NULL;
-  }
-  return (str_t *)str;
+    volatile str_t *str;
+
+    if (NULL == (str = str_alloca())) {
+        return NULL;
+    }
+    if (NULL == str_set(s, (str_t *)str)) {
+        return NULL;
+    }
+    return (str_t *)str;
 }
 
 inline void str_free(str_t *str)
 {
-  if (str->mem_type == HEAP && NULL != str) {
-  	if (NULL != str->s) {
-  		FREE(str->s);
-  	}
-  	FREE(str);
-  }
+    if (str->mem_type == HEAP && NULL != str) {
+        if (NULL != str->s) {
+            FREE(str->s);
+        }
+        FREE(str);
+    }
 }
 
 inline str_t *str_set(const char *s, str_t *str)
 {
-	size_t len;
-	
-	if (NULL == str) {
-		return NULL;
-	}
-	if (NULL == s) {
-		return str;
-	}
-	len = strlen(s);
-	if (0 == len) {
-		return str;
-	}
-	if (len > str->len) {
-	  FREE(str->s);
-	  if (str->mem_type == HEAP) {
-  	  if (NULL == (str->s = calloc(len + 1, sizeof(char)))) {
-  		  str_free(str);
-  		  return NULL;
-  	  }	    
-	  } else {
-	    if (NULL == (str->s = alloca((len + 1) * sizeof(char)))) {
-		    return NULL;
-	    }
+    size_t len;
+
+    if (NULL == str) {
+        return NULL;
     }
-	  str->len = len;
-  }
-	strncpy(str->s, s, len);
-	str->actual = len;
-	return str;
+    if (NULL == s) {
+        return str;
+    }
+    len = strlen(s);
+    if (0 == len) {
+        return str;
+    }
+    if (len > str->len) {
+        FREE(str->s);
+        if (str->mem_type == HEAP) {
+            if (NULL == (str->s = calloc(len + 1, sizeof(char)))) {
+                str_free(str);
+                return NULL;
+            }	    
+        } else {
+            if (NULL == (str->s = alloca((len + 1) * sizeof(char)))) {
+                return NULL;
+            }
+        }
+        str->len = len;
+    }
+    strncpy(str->s, s, len);
+    str->actual = len;
+    return str;
 }
 
 inline const char *str_get(str_t *str)
 {
-  return NULL == str ? NULL : str->s;
+    return NULL == str ? NULL : str->s;
 }
 
 static inline char *resize(size_t len, str_t *str)
 {
-  if (str->mem_type == HEAP) {
-	  if (NULL == (str->s = realloc(str->s, len))) {
-		  return NULL;
-	  }
-  } else {
-    char *ptr = str->s;
-    if (str->len * 2 + 1 < len) {
-      str->len = len;
+    if (str->mem_type == HEAP) {
+        if (NULL == (str->s = realloc(str->s, len))) {
+            return NULL;
+        }
+        str->len = len;
     } else {
-      str->len *= 2;
-      str->len += 1;
+        char *ptr = str->s;
+        if (str->len * 2 + 1 < len) {
+            str->len = len;
+        } else {
+            str->len *= 2;
+            str->len += 1;
+        }
+        if (NULL == (str->s = alloca(str->len * sizeof(char)))) {
+            return NULL;
+        }
+        strncat(str->s, ptr, strlen(ptr));
     }
-    if (NULL == (str->s = alloca(str->len * sizeof(char)))) {
-      return NULL;
-    }
-    strncat(str->s, ptr, strlen(ptr));
-  }
-	return str->s;
+    return str->s;
 }
 
 inline str_t *str_append(const char *add, str_t *dst)
 {
-	size_t add_len;
-	
-	if (NULL == add || 0 == strlen(add) || NULL == dst) {
-		return dst;
-	}
-	add_len = strlen(add) + 1;
-	if (dst->actual + add_len > dst->len) {
-		if (NULL == (dst->s = resize(dst->actual + add_len, dst))) {
-      FREE(dst);
-			return NULL;
-		}
-	}
-	strncat(dst->s, add, dst->len);
-  dst->actual += add_len;
-	return dst;
+    size_t add_len;
+
+    if (NULL == add || 0 == strlen(add) || NULL == dst) {
+        return dst;
+    }
+    add_len = strlen(add) + 1;
+    if (dst->actual + add_len > dst->len) {
+        if (NULL == (dst->s = resize(dst->actual + add_len, dst))) {
+            FREE(dst);
+            return NULL;
+        }
+    }
+    strncat(dst->s, add, dst->len);
+    dst->actual += add_len;
+    return dst;
 }
 
 inline str_t *str_join(str_t *add, str_t *dst)
 {
-	if (NULL == add || NULL == dst) {
-		return NULL;
-	}
-	return str_append(add->s, dst);
+    if (NULL == add || NULL == dst) {
+        return NULL;
+    }
+    return str_append(add->s, dst);
 }
 
 inline size_t str_length(str_t *str)
 {
-	return str == NULL ? 0 : str->actual;
+    return str == NULL ? 0 : str->actual;
 }
